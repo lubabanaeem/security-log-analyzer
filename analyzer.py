33392664a815b57ba24f04a2d34379734bf5f56a
Lubaba Naeem <naeemlubaba915@gmail.com>
2026-01-24 19:13:33 +0300

    Add failed login detection and IP aggregation

--- analyzer.py
+++ analyzer.py
@@ -1,8 +1,24 @@
 with open("sample.log","r") as file:
     logs = file.readlines()
 
-for lines in logs:
-    print(lines.strip())
+
+ip_count = {}
+for line in logs:
+    if "Failed login" in line:
+        my_list = line.split(" ")
+        ip = my_list[-1].strip()
+        
+        if ip in ip_count:
+            ip_count[ip] = ip_count[ip]+1
+        else:
+            ip_count[ip] = 1
+for values in ip_count:
+    print(values, "->",ip_count[values], "failed login attempts")
+   
+
+
+        
+
 
 
 
